4ea81522f8fa7240305620f10bb751f8a628f310
fahim <fahim1998108@gmail.com>
2025-03-22 04:41:38 +0530

    Update Order✅

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,95 +1,141 @@
 import Order from "../models/order.js";
-import { isCustomer } from "./userController.js";
+ import Product from "../models/product.js";
+ import { isCustomer } from "./userController.js";
 
-export async function createOrder(req,res){
-   
-    if(!isCustomer){
-      res.json({
-        message: "Please login as customer to create orders"
-      })
+ export async function createOrder(req, res) {
+  if (!isCustomer) {
+    res.json({
+      message: "Please login as customer to create orders",
+    });
+  }
+  try {
+    const latestOrder = await Order.find().sort({ orderId: -1 }).limit(1);
+    console.log(latestOrder);
+    let orderId;
+    if (latestOrder.length == 0) {
+      orderId = "CBC0001";
+    } else {
+      const currentOrderId = latestOrder[0].orderId;
+      const numberString = currentOrderId.replace("CBC", "");
+      const number = parseInt(numberString);
+      const newNumber = (number + 1).toString().padStart(4, "0");
+      orderId = "CBC" + newNumber;
     }
-  
-    try{
-      const latestOrder = await Order.find().sort({date : -1}).limit(1)
-  
-      let orderId
-  
-      if(latestOrder.length == 0){
-        orderId = "CBC0001"
-      }else{
-        const currentOrderId = latestOrder[0].orderId
-  
-        const numberString =  currentOrderId.replace("CBC","")
-  
-        const number = parseInt(numberString)
-  
-        const newNumber = (number + 1).toString().padStart(4, "0");
-  
-        orderId = "CBC" + newNumber
-      }
-  
-      const newOrderData = req.body
+    const newOrderData = req.body;
+ 
+     const newProductArray = [];
+ 
+     for (let i = 0; i < newOrderData.orderedItems.length; i++) {
+       const product = await Product.findOne({
+         productId: newOrderData.orderedItems[i].productId,
+       });
+ 
+       if (product == null) {
+         res.json({
+           message:
+             "Product with id " +
+             newOrderData.orderedItems[i].productId +
+             " not found",
+         });
+         return;
+       }
+ 
+       newProductArray[i] = {
+         name: product.productName,
+         price: product.lastPrice,
+         quantity: newOrderData.orderedItems[i].qty,
+         image: product.images[0],
+       };
+     }
+     console.log(newProductArray);
+     newOrderData.orderedItems = newProductArray;
 
-      const newProductArray = []
+     newOrderData.orderId = orderId;
+     newOrderData.email = req.user.email;
+ 
+     const order = new Order(newOrderData);
+     const savedOrder = await order.save();
+     res.json({
+      message: "Order created",
+       order : savedOrder
+     });
+   } catch (error) {
+     res.status(500).json({
+       message: error.message,
+     });
+   }
+ }
+ export async function getOrders(req, res) {
+   
      
-      for(let i=0;i<newOrderData.orderedItems.length;i++){
-  
-          const product = await Product.findOne({
-            productId : newOrderData.orderedItems[i].productId
-          })
-  
-  
-          if(product == null){
-            res.json({
-              message: "Product with id "+newOrderData.orderedItems[i].productId+" not found"
-            })
-            return
-          }
-  
-          newProductArray[i] = {
-            name : product.productName,
-            price : product.price,
-            quantity : newOrderData.orderedItems[i].quantity,
-            image : product.images[0]
-          }
-  
-      
-      }
-      console.log(newProductArray) 
-  
-      newOrderData.orderedItems = newProductArray
-   
-  
-      newOrderData.orderId = orderId
-      newOrderData.email = req.user.email
-  
-      const order = new Order(newOrderData)
-  
-      await order.save()
-  
+  try {
+    if (isCustomer(req)) {
+    const orders = await Order.find({ email: req.user.email });
+
+    res.json(orders);
+    return;
+    }else if(isAdmin(req)){
+      const orders = await Order.find({});
+      res.json(orders);
+      return;
+    }else{
       res.json({
-        message: "Order created"
-      })
-  
-  
-  
-    }catch(error){
-      res.status(500).json({
-        message: error.message
+        message: "Please login to view orders"
       })
     }
-  
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
   }
-  
-  export async function getOrders(req,res){
-    try{
-      const orders = await Order.find({email : req.user.email})
-  
-      res.json(orders)
-  
-    }catch(error){
-      res.status(500).json({
-        message: error.message
-      })
+}
+export async function getQuote(req, res) {
+   
+  try {
+    const newOrderData = req.body;
+
+    const newProductArray = [];
+
+    let total = 0;
+    let labeledTotal = 0;
+    console.log(req.body)
+
+    for (let i = 0; i < newOrderData.orderedItems.length; i++) {
+      const product = await Product.findOne({
+        productId: newOrderData.orderedItems[i].productId,
+      });
+
+      if (product == null) {
+        res.json({
+          message:
+            "Product with id " +
+            newOrderData.orderedItems[i].productId +
+            " not found",
+        });
+        return;
+      }
+      labeledTotal += product.price * newOrderData.orderedItems[i].qty;
+      total += product.lastPrice * newOrderData.orderedItems[i].qty;
+      newProductArray[i] = {
+        name: product.productName,
+        price: product.lastPrice,
+        labeledPrice: product.price,
+        quantity: newOrderData.orderedItems[i].qty,
+        image: product.images[0],
+      };
     }
+    console.log(newProductArray);
+    newOrderData.orderedItems = newProductArray;
+    newOrderData.total = total;
+
+    res.json({
+      orderedItems: newProductArray,
+      total: total,
+      labeledTotal: labeledTotal,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
   }
+}
